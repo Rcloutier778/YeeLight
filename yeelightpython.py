6bcf2ae49ea97459ad180f714171ad6c8230b730
Richard <rcloutier778@gmail.com>
2018-11-03 11:39:54 -0400

    Cleaned up and added todos

--- yeelightpython.py
+++ yeelightpython.py
@@ -22,13 +22,16 @@ eastern = pytz.timezone('EST')
 """
 1) autoset on wakeup from lan
 2) cortana integration
+3) hourly light temp color updates
+4) better sunrise transition.
+    Currently starts at low red, turns off for whatever reason, then goes from 1500k to 4800k temp color.
+    Need to do this in hue/sat or rgb instead I think
+5) Brightness slider in system tray
+6) Left click system tray for lights toggle
 """
-
-
 
 def main():
     #print(desk.get_properties())
-    #{'power': 'on', 'bright': '80', 'ct': '3170', 'rgb': '7864115', 'hue': '100', 'sat': '80', 'color_mode': '2', 'flowing': '0', 'delayoff': '0', 'music_on': '0', 'name': None}
     if len(sys.argv) == 1:
         print("No arguments.")
         return
@@ -58,36 +61,23 @@ def sunrise():
 def brightness(val):
     for i in b:
         i.set_brightness(val)
-        
 
 def day(duartion=3000):
     on()
-    for i in b:
-        #i.set_hsv(36,100)
-        i.start_flow(yeelight.Flow(count=1,
-                                   action=yeelight.Flow.actions.stay,
-                                   transitions=[yeelight.TemperatureTransition(4800, duartion, 80)]))
+    colorTempFlow(4800, duartion, 80)
 
 def dusk(duartion=3000):
     on()
-    for i in b:
-        #i.set_hsv(36,100)
-        i.start_flow(yeelight.Flow(count=1,
-                                   action=yeelight.Flow.actions.stay,
-                                   transitions=[yeelight.TemperatureTransition(3000, duartion, 80)]))
+    colorTempFlow(3000, duartion, 80)
         
 def night(duartion=3000):
     on()
-    for i in b:
-        i.start_flow(yeelight.Flow(count=1,
-                                   action=yeelight.Flow.actions.stay,
-                                   transitions=[yeelight.TemperatureTransition(2500, duartion, 80)]))
+    colorTempFlow(2500, duartion, 80)
+
 def sleep(duartion=3000):
     on()
-    for i in b:
-        i.start_flow(yeelight.Flow(count=1,
-                                   action=yeelight.Flow.actions.stay,
-                                   transitions=[yeelight.TemperatureTransition(1500, duartion, 20)]))
+    colorTempFlow(1500,duartion,20)
+
 def off():
     for i in b:
         i.turn_off()
@@ -100,13 +90,29 @@ def toggle():
     for i in b:
         i.toggle()
 
+def colorTempFlow(temperature=6700,duration=3000, brightness=80):
+    #control all lights at once
+    #makes things look more condensed
+    transition=yeelight.TemperatureTransition(degrees=temperature,duration=duration,brightness=brightness)
+    for i in b:
+        i.start_flow(yeelight.Flow(count=1,
+                                   action=yeelight.Flow.actions.stay,
+                                   transitions=[transition]))
+
+
 def lightTime():
     #TODO
-    #set light level based on time of day
+    #set light level based on time of day, hour by hour to get smoother transition than day/dusk/night/sleep
     #day=datetime.time.
     #time.time()
     pass
 
+def discoverBulbs():
+    bulbs=yeelight.discover_bulbs()
+    for bulb in bulbs:
+        print(bulb)
+    
+    
 def autoset():
     #set light level when computer is woken up, based on time of day
     rn=datetime.datetime.now(eastern)
@@ -141,13 +147,15 @@ if __name__ == "__main__":
             night()
         def systraysleep(SysTrayIcon):
             sleep()
+        #TODO on left click, toggle lights
+        #TODO brightness slider
         
         menu_options= (('Day', next(ico), systrayday),
                        ('Dusk', next(ico), systraydusk),
                        ('Night', next(ico), systraynight),
                        ('Sleep', next(ico), systraysleep))
         
-        sysTray.SysTrayIcon(next(ico),'hover',menu_options, on_quit=print("bye"))
+        sysTray.SysTrayIcon(next(ico),'Light controller',menu_options, on_quit=print("bye"))
     else:
         #run the python script
         main()
