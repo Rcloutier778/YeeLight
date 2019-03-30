a09a50b8c97568c89a334d8e964bff8bbcf28736
Richard <rcloutier778@gmail.com>
2019-03-30 11:34:32 -0400

    Lots of improvements.

--- yeelightpython.py
+++ yeelightpython.py
@@ -7,8 +7,11 @@ import datetime
 import SysTrayIcon as sysTray
 import logging
 import os
+
+os.chdir('C:/Users/Richard/Documents/Coding Projects/YeeLight/')
+
 log = logging.getLogger('log')
-logging.basicConfig(filename='C:/Users/Richard/Documents/Coding Projects/YeeLight/log.log',
+logging.basicConfig(filename=os.getcwd()+'/log.log',
                     filemode='a',
                     format='%(asctime)s %(name)s %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %I:%M:%S%p',
@@ -17,7 +20,7 @@ logging.basicConfig(filename='C:/Users/Richard/Documents/Coding Projects/YeeLigh
 stand = yeelight.Bulb("10.0.0.5")
 desk = yeelight.Bulb("10.0.0.10")
 b=[stand,desk]
-commands=['dusk','day','night','sleep', 'off', 'on','toggle','sunrise','autoset']
+commands=['dusk','day','night','sleep', 'off', 'on','toggle','sunrise','autoset','logon']
 allcommands=commands + ['bright','brightness','rgb']
 
 
@@ -26,22 +29,22 @@ allcommands=commands + ['bright','brightness','rgb']
 1) autoset on wakeup from lan
 2) cortana integration
 3) hourly light temp color updates
-4) better sunrise transition.
-    Currently starts at low red, turns off for whatever reason, then goes from 1500k to 4800k temp color.
-    Need to do this in hue/sat or rgb instead I think
-5) Brightness slider in system tray
+4) Brightness slider in system tray
 """
 
 def main():
     #print(desk.get_properties())
-    #responses=[]
-    #for i in range(4):
-    #     responses.append(os.system("ping -n 1 10.0.0.7"))
-    #if 0 not in responses: #timeout, phone not present.
-    #    print("Phone not present.")
-    #    log.warning("Phone not present.")
-    #    off()
-    #    return
+    
+    '''
+    import subprocess
+
+    response=subprocess.getstatusoutput('ping -n 2 10.0.0.7')
+    if 'time=' not in response[1]: #timeout, phone not present.
+        print("Phone not present.")
+        log.warning("Phone not present.")
+        off()
+        return
+    '''
     if len(sys.argv) == 1:
         print("No arguments.")
         log.warning('No arguments.')
@@ -50,7 +53,8 @@ def main():
         cmd=sys.argv[1].lower()
         if cmd in allcommands:
             if cmd in commands:
-                log.info(cmd)
+                if cmd != 'autoset':
+                    log.info(cmd)
                 globals()[cmd]()
         elif cmd in ['bright','brightness']:
             if type(sys.argv[2]) == int:
@@ -62,7 +66,7 @@ def main():
 
 def sunrise():
     #Prevent autoset from taking over
-    with open('C:/Users/Richard/Documents/Coding Projects/YeeLight/manualOverride.txt', 'w+') as f:
+    with open(os.getcwd()+'/manualOverride.txt', 'w+') as f:
         f.write(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     overallDuration=1200000 #1200000 == 20 min
     on()
@@ -72,10 +76,10 @@ def sunrise():
     print(desk.get_properties())
     time.sleep(1)
     
-    transitions=[yeelight.RGBTransition(red=255, green=167, blue=0, duration=overallDuration*0.5, brightness=80),
-                yeelight.TemperatureTransition(degrees=3200, duration=overallDuration*0.5, brightness=80)]
-
-    transitions = [yeelight.HSVTransition(hue=39, saturation=100, duration=overallDuration * 0.5, brightness=80),                   yeelight.TemperatureTransition(degrees=3200, duration=overallDuration * 0.5, brightness=80)]
+    transitions = [yeelight.HSVTransition(hue=39, saturation=100,
+                    duration=overallDuration * 0.5, brightness=80),
+                   yeelight.TemperatureTransition(degrees=3200,
+                    duration=overallDuration * 0.5, brightness=80)]
     
     for i in b:
         i.start_flow(yeelight.Flow(count=1,action=yeelight.Flow.actions.stay,transitions=transitions))
@@ -87,11 +91,13 @@ def brightness(val):
 def day(duration=3000,auto=False):
     if not auto:
         on()
-    colorTempFlow(3200, duration, 80)
+    #3200
+    colorTempFlow(4000, duration, 80)
 
 def dusk(duration=3000,auto=False):
     if not auto:
         on()
+    #3000
     colorTempFlow(3000, duration, 80)
     
 def night(duration=3000,auto=False):
@@ -145,42 +151,45 @@ def discoverBulbs():
     bulbs=yeelight.discover_bulbs()
     for bulb in bulbs:
         print(bulb)
+
+        
+        
+def logon():
+    on()
+    autoset(autosetDuration=3000)
+    return
     
-    
-def autoset():
-    autosetDuration=10000
-    
+def autoset(autosetDuration=10000):
+    if all(x.get_properties()['power']=='off' for x in b):
+        log.info('Power is off, cancelling autoset')
+        return -1
     #Check if system tray has been used recently to override autoset
-    with open('C:/Users/Richard/Documents/Coding Projects/YeeLight/manualOverride.txt', 'r') as f:
+    with open(os.getcwd()+'/manualOverride.txt', 'r') as f:
         ld = f.read().strip()
     if datetime.datetime.strptime(ld,'%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=1) > datetime.datetime.utcnow():
         print("SystemTray used recently, canceling autoset")
         log.info("SystemTray used recently, canceling autoset")
         return -1
     
+    
     #set light level when computer is woken up, based on time of day
     rn=datetime.datetime.now() # If there is ever a problem here, just use time.localtime()
     now=datetime.time(rn.hour,rn.minute,0)
-    print(now)
-    dayrange = ["6:50:AM", "5:00:PM"]
+    print('now:',now)
+    dayrange = ["6:50:AM", "6:00:PM"]
     if time.localtime().tm_wday in [5, 6]: #weekend
         print("weekend")
         dayrange[0] = "8:30:AM"
-    duskrange=[dayrange[1],"8:00:PM"]
-    nightrange=[duskrange[1],"10:00:PM"]
-    sleeprange=[nightrange[1],"11:30:PM"]
+    duskrange=[dayrange[1],"7:45:PM"]
+    nightrange=[duskrange[1],"8:30:PM"]
+    sleeprange=[nightrange[1],"11:00:PM"]
     DNDrange=[sleeprange[1],dayrange[0]]
     ranges=[dayrange,duskrange,nightrange,sleeprange,DNDrange]
+    
     for r in ranges:
         for rr in range(0,2):
-            t=r[rr].split(":")
-            if t[2] == 'PM':
-                t[0] = int(t[0]) + 12
-            else:
-                t[0] = int(t[0])
-            t[1]=int(t[1])
-            r[rr]=datetime.time(t[0],t[1],0)
-            datetime.time()
+            t = datetime.datetime.strptime(r[rr], "%I:%M:%p")
+            r[rr] = datetime.time(t.hour,t.minute,0)
     for i in ranges:
         print(i)
     print(dayrange[0].strftime("%H:%M:%S"))
@@ -210,27 +219,31 @@ if __name__ == "__main__":
     #Run the system tray app
     if len(sys.argv) > 1 and sys.argv[1].lower() == 'systray':
         import glob, itertools
-        ico=itertools.cycle(glob.glob('C:/Users/Richard/Documents/Coding Projects/YeeLight/icons/*.ico'))
+        ico=itertools.cycle(glob.glob(os.getcwd()+'/icons/*.ico'))
 
         def systrayday(SysTrayIcon):
             day()
-            with open('C:/Users/Richard/Documents/Coding Projects/YeeLight/manualOverride.txt','w+') as f:
+            with open(os.getcwd()+'/manualOverride.txt','w+') as f:
                 f.write(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
         def systraydusk(SysTrayIcon):
             dusk()
-            with open('C:/Users/Richard/Documents/Coding Projects/YeeLight/manualOverride.txt','w+') as f:
+            with open(os.getcwd()+'/manualOverride.txt','w+') as f:
                 f.write(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
         def systraynight(SysTrayIcon):
             night()
-            with open('C:/Users/Richard/Documents/Coding Projects/YeeLight/manualOverride.txt','w+') as f:
+            with open(os.getcwd()+'/manualOverride.txt','w+') as f:
                 f.write(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
         def systraysleep(SysTrayIcon):
             sleep()
-            with open('C:/Users/Richard/Documents/Coding Projects/YeeLight/manualOverride.txt','w+') as f:
+            with open(os.getcwd()+'/manualOverride.txt','w+') as f:
                 f.write(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
         def systraytoggle(SysTrayIcon):
             toggle()
-        #TODO on left click, toggle lights
+            rn = datetime.datetime.now()
+            now=datetime.time(rn.hour, rn.minute, 0)
+            if datetime.time(22,30) <= now or now < datetime.time(1,0): #11:30, 1:00
+                with open(os.getcwd() + '/manualOverride.txt', 'w+') as f:
+                    f.write(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
         #TODO brightness slider
         
         menu_options= (('Day', next(ico), systrayday),
